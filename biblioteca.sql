27f801f6e3b9018181f91f33002b9d7252adfe08
Murilo Poltronieri Batista De Campos <49900491874@>
2024-02-07 16:59:14 -0300

    sql

---
+++ biblioteca.sql
@@ -0,0 +1,11 @@
+/*CREATE DATABASE biblioteca
+CREATE TABLE Autor (
+	idAutor INT auto_increment primary key,
+    nome varchar(50) not null,
+    dataNascimento date,
+    biografia text
+)
+*/
+create table Usuario (
+
+)
